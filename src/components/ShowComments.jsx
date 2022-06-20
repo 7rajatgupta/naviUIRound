1099e0a4a689f67fbffe2bb1c1109161fe73f588
Rajat Gupta <7rgdev@gmail.com>
2022-06-20 17:44:26 +0530

    refactor

--- src/components/ShowComments.jsx
+++ src/components/ShowComments.jsx
@@ -1,55 +1,78 @@
-import './App.css';
-import { Flex, Heading } from '@chakra-ui/react';
-import CommentWidget from './components/CommentWidget';
+import { Flex, Text, Button, Input } from '@chakra-ui/react';
 import { useState } from 'react';
-import ShowComments from './components/ShowComments';
-import { deleteRecursively, getNewCommentDS } from './utils/commentHelper';
+import { getNewCommentDS } from '../utils/commentHelper';
+let threadlevel = 0;
 
-function App() {
-  const [comments, setComments] = useState([]);
+const ShowComments = ({ comment, set, level, deleteComment }) => {
+  const [isReply, setIsReply] = useState(false);
+  const [replyText, setReplyText] = useState('');
 
-  function modifyComment(comment) {
-    let updatedComments = comments;
-    updatedComments.map((c, id) => {
-      if (c.id === comment.id) {
-        c.threads = comment.threads;
-      }
-      return true;
-    });
-    setComments(updatedComments);
-  }
-  function deleteCommentWithId(id) {
-    const updatedComments = deleteRecursively(comments, id);
-    setComments(updatedComments);
-  }
-
-  function addTopLevelComment(text) {
+  function addCommentToId(text, id) {
     const newComment = getNewCommentDS(text);
-    setComments([...comments, newComment]);
+    let updatedComment = comment;
+    updatedComment.threads = [...updatedComment.threads, newComment];
+    set(updatedComment);
+    setReplyText('');
+    setIsReply(false);
   }
-
+  // function deleteCommentFromId(id) {
+  //   let updatedComments = comment.threads.filter((c) => c.id !== id);
+  // }
+  const threadedComments = (comment.threads || []).map((c, id) => {
+    return (
+      <ShowComments
+        comment={c}
+        set={set}
+        key={id}
+        level={++threadlevel}
+        deleteComment={deleteComment}
+      />
+    );
+  });
   return (
-    <div className='App'>
-      <Heading fontSize={'3xl'} mb={5} mt={2}>
-        Navi UI 2 Coding Round
-      </Heading>
-      <Flex direction={'column'}>
-        <CommentWidget add={addTopLevelComment} />
-        <Flex direction={'column'}>
-          {comments?.map((c, id) => {
-            return (
-              <ShowComments
-                comment={c}
-                key={id}
-                set={modifyComment}
-                deleteComment={deleteCommentWithId}
-              />
-            );
-          })}
-        </Flex>
+    <Flex direction={'column'} ml={level * 3}>
+      <Flex gap={'1rem'} mb={5}>
+        <Text>{comment.text}</Text>
+        <Button
+          colorScheme={'red'}
+          variant={'outline'}
+          size={'xs'}
+          onClick={() => deleteComment(comment.id)}
+        >
+          Delete
+        </Button>
+        {isReply === true ? (
+          <Flex gap={'1rem'}>
+            <Input
+              placeholder={'Enter your reply...'}
+              value={replyText}
+              size={'sm'}
+              onChange={(e) => setReplyText(e.target.value)}
+            />
+            <Button
+              colorScheme={'blue'}
+              variant={'solid'}
+              size={'sm'}
+              disabled={replyText.length > 0 ? false : true}
+              onClick={() => addCommentToId(replyText, comment.id)}
+            >
+              Reply
+            </Button>
+          </Flex>
+        ) : (
+          <Button
+            colorScheme={'blue'}
+            variant={'outline'}
+            size={'xs'}
+            onClick={() => setIsReply(true)}
+          >
+            Reply
+          </Button>
+        )}
       </Flex>
-    </div>
+      {threadedComments}
+    </Flex>
   );
-}
+};
 
-export default App;
+export default ShowComments;
